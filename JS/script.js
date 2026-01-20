8fd6d567be85735ea7867e76844a2ed70c2dd1b2
rlan <rladnrider77@gmail.com>
2026-01-20 23:40:21 +0900

    update

--- JS/script.js
+++ JS/script.js
@@ -2,42 +2,67 @@
 const posts = [
   {
     id: 1,
-    thumbnali: '../img/JS.png',
-    title: 'JavaScript: 기초',
+    thumbnail: "../img/JS.png",
+    title: "JavaScript: 기초",
     info: { date: "2개월 전", view: 30, comment: 15 },
-    summary: 'Content contetn coneten',
-    tag: ["css", "javascript", "react"]
+    summary: "Content contetn coneten",
+    tag: ["css", "javascript", "react"],
+    type: "post"
   },
   {
     id: 2,
-    thumbnali: '../img/JS.png',
-    title: 'CSS: 기초',
+    thumbnail: "../img/JS.png",
+    title: "CSS: 기초",
     info: { date: "2개월 전", view: 30, comment: 15 },
-    summary: 'Content contetn coneten',
-    tag: ["react"]
+    summary: "Content contetn coneten",
+    tag: ["react"],
+    type: "post"
   },
   {
     id: 3,
-    thumbnali: '../img/JS.png',
-    title: 'HTML: 기초',
+    thumbnail: "../img/JS.png",
+    title: "HTML: 기초",
     info: { date: "2개월 전", view: 30, comment: 15 },
-    summary: 'Content contetn coneten',
-    tag: ["css", "html", "vue"]
-  }
+    summary: "Content contetn coneten",
+    tag: ["css", "html", "vue"],
+    type: "post"
+  },
+  {
+    id: 4,
+    thumbnail: "../img/JS.png",
+    title: "User",
+    info: { date: "2개월 전", view: 30, comment: 15 },
+    summary: "Hi, my name is Kim",
+    tag: ["css", "html", "vue"],
+    type: "user"
+  },
+  {
+    id: 5,
+    thumbnail: "../img/JS.png",
+    title: "HTML: 기초",
+    info: { date: "2개월 전", view: 30, comment: 15 },
+    summary: "안녕하세요, Kenny입니다.",
+    tag: ["css", "html", "vue"],
+    type: "user"
+  },
 ];
 
 // 상태
-let activeTag = 'all';
+let state = {
+  posts: posts,
+  activeTag: 'all',
+  activeCategory: 'post',
+}
 
 // 게시글 리스트
 const postList = document.querySelector('.post__list');
 
 // 렌더링
-function renderPosts(posts) {
+function renderPosts(state) {
   postList.innerHTML = '';
 
   // 태그 필터링
-  const filteredPosts = activeTag === 'all' ? posts : posts.filter(post => post.tag.includes(activeTag));
+  const filteredPosts = state.activeTag === 'all' ? state.posts : state.posts.filter(post => post.tag.includes(state.activeTag));
 
   filteredPosts.forEach((post) => {
     const article = document.createElement('article');
@@ -47,7 +72,7 @@ function renderPosts(posts) {
 
     article.innerHTML = `
       <div class="post__thumbnail">
-        <img src="${post.thumbnali}" alt="게시글 썸네일">
+        <img src="${post.thumbnail}" alt="게시글 썸네일">
       </div>
       <div class="post__content">
         <h2 class="post__title">
@@ -76,16 +101,27 @@ function renderPosts(posts) {
 };
 
 
-// 태그 버튼
+// 태그 이벤트
 const tagButtons = document.querySelectorAll(".sidebar__tag-item button");
 
 tagButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    activeTag = btn.dataset.tag;
-    renderPosts(posts);
+    state.activeTag = btn.dataset.tag;
+    renderPosts(state);
   });
 });
 
+// 검색 필터 이벤트
+const searchForm = document.querySelector(".header__search");
+const headerSelect = document.querySelector(".header__select");
+
+searchForm.addEventListener("submit", (e) => {
+  state.activeCategory = headerSelect.value;
+  renderPosts({ posts: state.posts })
+});
+
+
+// 실행
 document.addEventListener('DOMContentLoaded', () => {
-  renderPosts(posts);
+  renderPosts(state);
 });
